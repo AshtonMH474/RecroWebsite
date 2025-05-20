6995c775788401c2e2493c624c5a597f86e02680
Ashton Howard <ashtonhowa@gmail.com>
2025-05-20 14:28:48 -0400

    gears are moving and background is fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,37 @@
+'use client'
+import { useRef } from "react";
+import GearIcon from "./components/GearIcon";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 export default function Home() {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, 360]);
+
   return (
-    <div
-    className="background Home"
-    >
- 
+    <div>
+      {/* Full viewport section with a fixed background image */}
+      <div
+        ref={ref}
+        className=" background Home h-screen bg-fixed bg-center bg-cover flex items-center justify-end"
+       
+      >
+        <motion.div style={{ rotate }} className="mr-10">
+          <GearIcon className="h-40 w-40 text-white" />
+        </motion.div>
+
+          
+      </div>
+      <div className="test"></div>
+
+      {/* More content below so you can scroll */}
+  
     </div>
   );
 }
+
