cde487016bf7ea416e9034b287ad43d560f05747
Ashton Howard <ashtonhowa@gmail.com>
2025-06-17 14:38:05 -0400

    testing cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-        });
-        return config;
-    },
+  output: 'export', // 👈 tells Next.js to generate static export to /out
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+
   reactStrictMode: true,
-  async rewrites(){
-        return [
-            {
-                source:'/admin',
-                destination:'/admin/index.html'
-            }
-        ]
-    }
+
+  async rewrites() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
