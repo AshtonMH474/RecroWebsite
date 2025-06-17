0fdf598a8b0ad03253620750aa1578cf180c43a1
Ashton Howard <148167976+AshtonMH474@users.noreply.github.com>
2025-06-17 15:06:40 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 👈 tells Next.js to generate static export to /out
-
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-
+    webpack(config) {
+        config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+        });
+        return config;
+    },
   reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: '/admin',
-        destination: '/admin/index.html',
-      },
-    ];
-  },
+  async rewrites(){
+        return [
+            {
+                source:'/admin',
+                destination:'/admin/index.html'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
