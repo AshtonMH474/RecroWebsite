d827f0cf0d245dad9a4f0e770e72bf6b3866f186
Ashton Howard <ashtonhowa@gmail.com>
2025-06-23 16:22:51 -0400

    seeing if this would work

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
       </Head>
         <ExpertiseProvider>
           <Component {...pageProps} />
