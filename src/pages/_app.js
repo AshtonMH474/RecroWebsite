f611016d9a7ccf2dfb1a4932df6050e8388ddd9d
Ashton Howard <ashtonhowa@gmail.com>
2025-06-24 22:28:33 -0400

    testing to see if its the useeffect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,27 +2,9 @@ import Head from "next/head";
 import "@/styles/globals.css";
 import "@/styles/gears.css";
 import { ExpertiseProvider } from "@/context/ExpertiseContext";
-import { useEffect } from "react";
+
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-    if (!isMobile) return; // 🔒 Only apply fix on mobile
-
-    const setVH = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    setVH();
-    window.addEventListener("resize", setVH);
-    window.addEventListener("orientationchange", setVH);
-
-    return () => {
-      window.removeEventListener("resize", setVH);
-      window.removeEventListener("orientationchange", setVH);
-    };
-  }, []);
 
   return (
     <>
