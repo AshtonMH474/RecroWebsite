4474e4420b511814900767fa31961990e78ac300
Ashton Howard <ashtonhowa@gmail.com>
2025-07-09 13:31:11 -0400

    testing if its my meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -28,7 +28,7 @@ export default function App({ Component, pageProps }) {
       const widthDiff = Math.abs(currentWidth - lastSize.current.width);
       const heightDiff = Math.abs(currentHeight - lastSize.current.height);
 
-      if (widthDiff > 100 || heightDiff > 100) {
+      if (widthDiff > 150 || heightDiff > 150) {
         const newVh = currentHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${newVh}px`);
         lastSize.current = { width: currentWidth, height: currentHeight };
@@ -42,7 +42,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
         <div
       style={{
