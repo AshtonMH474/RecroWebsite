426771caeca58c5a514ce65704408c9500406eb8
Ashton Howard <ashtonhowa@gmail.com>
2025-07-09 16:14:51 -0400

    fixed bug

--- src/components/Landing2.jsx
+++ src/components/Landing2.jsx
@@ -5,7 +5,7 @@ import { TinaMarkdown } from "tinacms/dist/rich-text"
 function Landing2(props){
     
     return(
-    <div style={{ minHeight: '100dvh' }} className="landing  lg:-mt-20 flex flex-col lg:flex-row  items-center justify-center  w-full gap-x-22 ">
+    <div  className="landing h-screen lg:-mt-20 flex flex-col lg:flex-row  items-center justify-center  w-full gap-x-22 ">
         <div className="lg:pl-20">
             {props.headingLanding2 && (
                 <div className='w-90 md:w-100' data-tina-field={tinaField(props,'headingLanding2')}>
