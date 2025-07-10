9e582795f6f3d0ab966cec84d0c4fa0f47080769
Ashton Howard <ashtonhowa@gmail.com>
2025-07-10 18:27:38 -0400

    what happnes if u move bg outside conatiner

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -47,8 +47,9 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
-      <div className="app-wrapper">
+      
         <BG/>
+        <div className="app-wrapper">
         <Component {...pageProps} />
       </div>
 
