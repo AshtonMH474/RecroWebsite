86856cdb8cf5e611f94a332262eb13f7c754ed6f
Ashton Howard <ashtonhowa@gmail.com>
2025-08-27 10:18:47 -0400

    testing a better BG

--- src/components/BG.jsx
+++ src/components/BG.jsx
@@ -10,29 +10,32 @@ import { useRef } from "react"
     });
 
     return(
+
+      <div  className="fixed top-0 left-0 w-full h-full -z-20">
         <div
         ref={ref}
         style={{height:'100dvh', minHeight:'100dvh'}}
         className="background Home  overflow-hidden bg-fixed bg-center bg-cover  bg-contain flex flex-col items-end"
          >
         
-            <motion.div  style={{ rotate, transformOrigin: "center center"}} className="mr-10 gear1">
+            <motion.div  style={{ rotate, transformOrigin: "center center"}} className="mr-10 gear1 will-change-transform translate-z-0">
               <GearIcon className="h-80 w-80 text-black" />
             </motion.div>
-            <motion.div style={{ rotate, transformOrigin: "center center"}} className="mr-10 gear2">
+            <motion.div style={{ rotate, transformOrigin: "center center"}} className="mr-10 gear2 will-change-transform translate-z-0">
               <GearIcon className="h-50 w-50 " />
             </motion.div>
-            <motion.div style={{ rotate, transformOrigin: "center center" }} className="mr-10 gear3">
+            <motion.div style={{ rotate, transformOrigin: "center center" }} className="mr-10 gear3 will-change-transform translate-z-0">
               <GearIcon className="h-65 w-65 text-black" />
             </motion.div>
-            <motion.div style={{ rotate, transformOrigin: "center center" }} className="mr-10 gear4">
+            <motion.div style={{ rotate, transformOrigin: "center center" }} className="mr-10 gear4 will-change-transform translate-z-0">
               <GearIcon className="h-80 w-80 " />
             </motion.div>
-            <motion.div style={{ rotate, transformOrigin: "center center" }} className="mr-10 gear5">
+            <motion.div style={{ rotate, transformOrigin: "center center" }} className="mr-10 gear5 will-change-transform translate-z-0">
               <GearIcon className="h-105 w-105 text-black" />
             </motion.div>
        
           
+      </div>
       </div>
 )
 
