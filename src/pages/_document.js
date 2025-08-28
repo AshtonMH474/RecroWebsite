60b387e97c2feec662cc7f7d55fc9dce9fb3fcb1
Ashton Howard <ashtonhowa@gmail.com>
2025-08-28 12:04:14 -0400

    seeing if ist teh ico

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,12 +6,12 @@ export default function Document() {
       <Head>
         {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/> */}
 
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        {/* <link rel="icon" href="/favicon.ico" type="image/x-icon" /> */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
         
 
         <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png"/>
-<link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png"/>
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png"/>
         <script
           type="text/javascript"
           id="hs-script-loader"
