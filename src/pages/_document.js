0d5def16e182cb5b5fd46e7d3f810706b2f698c4
Ashton Howard <ashtonhowa@gmail.com>
2025-08-28 11:40:42 -0400

    seeing if this fixes firefox and chrome apple works now

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
         
