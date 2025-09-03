782702611cd9113975c34e437a1e6f9de62ec5d1
Ashton Howard <ashtonhowa@gmail.com>
2025-09-03 11:54:37 -0400

    removing console.log

--- src/components/SolutionsGrid/SolutionModal.jsx
+++ src/components/SolutionsGrid/SolutionModal.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function PdfModal({ solution, onClose }) {
   const pdfUrl = solution.mainPdf;
-  console.log(solution)
+  
 
   useEffect(() => {
 
