bbdd43a993614710a1e66e7aae8fab3afe45b03e
Ashton Howard <ashtonhowa@gmail.com>
2026-01-23 18:21:15 -0500

    fix(ui): added radial-graident in html

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,20 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         {/* safari */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
 
-
         {/* chrome and firefox */}
-        <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png"></link>
-        
-
 
         <script
           type="text/javascript"
@@ -24,7 +23,7 @@ export default function Document() {
           src="//js.hs-scripts.com/4374141.js"
         ></script>
       </Head>
-      <body >
+      <body>
         <Main />
         <NextScript />
       </body>
